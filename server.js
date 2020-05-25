a2e23c177a3957721aedc06d2f37bacabb7d195b
Balázs Salfay <balazs.salfay@gmail.com>
2020-05-25 11:43:50 +0200

    heroku database config

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const profile = require("./controllers/profile");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-corrugated-64860",
-    user: "postgres",
-    password: "admin",
-    database: "smartbrain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
