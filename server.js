3b1e0c1407f45a2a583a3dab084c3512e5732201
Balázs Salfay <balazs.salfay@gmail.com>
2020-05-25 11:04:05 +0200

    root get request replaced

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
 app.post("/signin", signin.handleSignIn(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
 app.get("/profile/:id", profile.handleProfileGet(db));
